32cecf29bd61a7d6ca09f327a5d7eec8712ddede
unknown <ibrahimdeniharyanto@gmail.com>
2022-08-19 08:04:35 +0700

    menambahkan detail project dr day 5, karna di day 6 tidak ada perintah menambahkan blog detail

--- index.js
+++ index.js
@@ -7,21 +7,14 @@ const port = 8000
 app.set('view engine', 'hbs')//set view engine hbs
 app.use('/assets', express.static(__dirname + '/assets'))//biar bisa baca path folder assets
 
-app.get('/home/:name', function(request,response){
-    let id = request.params.name
-    console.log(id);
-
-    response.render("home", {
-        id,
-        title : "Selamat Datang di Web saya guys!"
-
-
-
-
-    })
+app.get('/home', function(request,response){
+    response.render("home")
 })
 app.get('/add-project', function(request,response){
     response.render("add-project")
+})
+app.get('/blog-detail', function(request,response){
+    response.render("blog-detail")
 })
 app.get('/contact', function(request,response){
     response.render("contact")
